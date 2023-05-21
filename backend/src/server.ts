f5e6dbe7e666e58e19e933553ae31a213b8226ce
Samuel Silva <samuel7silva@outlook.com>
2023-05-21 12:44:33 -0300

    feat: mobile: create new memory page

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,7 +21,7 @@ app.register(memoriesRoutes)
 app
   .listen({
     port: 3333,
-    // host: '0.0.0.0',
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('HTTP server running on http://localhost:3333')
