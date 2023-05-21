1e4f28ea17957878e0afd05b2690a2a30d0f9289
Samuel Silva <samuel7silva@outlook.com>
2023-05-21 13:51:36 -0300

    feat: backend: create upload files route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads/',
+})
 
 app.register(cors, {
   origin: true,
@@ -17,11 +27,12 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
     port: 3333,
-    host: '0.0.0.0',
+    // host: '0.0.0.0',
   })
   .then(() => {
     console.log('HTTP server running on http://localhost:3333')
