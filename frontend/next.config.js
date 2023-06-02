b29ffecf4d3afa2072d11a8416966e26fa82aa9a
Samuel Silva <samuel7silva@outlook.com>
2023-06-02 11:29:42 -0300

    fix: frontend: imagekit domain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'localhost', '10.0.0.187'],
+    domains: [
+      'avatars.githubusercontent.com',
+      'localhost',
+      '10.0.0.187',
+      'ik.imagekit.io',
+    ],
   },
 }
 
